4c8d22f1273d1f3af6549026b8953b2cf785ff1e
renobowen@gmail.com <rfbowen@rfbowen-PC.(none)>
2012-02-17 16:56:42 -0800

    Added prototype for stage tracker
    Region at top of the screen indicates stage tracker

--- OFWGKTA/OFWGKTA/ValueConverters.cs
+++ OFWGKTA/OFWGKTA/ValueConverters.cs
@@ -8,18 +8,103 @@ using System.Windows.Media;
 
 namespace OFWGKTA
 {
-    [ValueConversion(typeof(float), typeof(float))]
-    public class CoordinateConverter : IValueConverter
+    [ValueConversion(typeof(float), typeof(Color))]
+    public class StageConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            float param = 640;
             float coord = (float)value;
-            return coord - 10;
+            if (coord < param*.55 && coord > param*.45)
+            {
+                return Colors.Green;
+            }
+            else
+            {
+                return Colors.Red;
+            }
         }
     
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             float translatedCoord = (float)value;
+            return translatedCoord + 10;
+        }
+    }
+    [ValueConversion(typeof(float), typeof(float))]
+    public class CoordinateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float param;
+            if (parameter != null)
+            {
+                param = (float)parameter;
+            }
+            else
+            {
+                param = -10;
+            }
+
+            float coord = (float)value;
+            return coord + param;
+        }
+    
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float translatedCoord = (float)value;
+            return translatedCoord + 10;
+        }
+    }
+
+    [ValueConversion(typeof(float), typeof(float))]
+    public class NormalizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float param;
+            if (parameter != null)
+            {
+                param = (float)parameter;
+            }
+            else
+            {
+                param = 640;
+            }
+
+            float coord = (float)value;
+            return coord/param;
+        }
+    
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float translatedCoord = (float)value;
+            return translatedCoord + 10;
+        }
+    }
+
+    [ValueConversion(typeof(string), typeof(double))]
+    public class DoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double param;
+            if (parameter != null)
+            {
+                param = (double) double.Parse((string)parameter);
+            }
+            else
+            {
+                param = -10;
+            }
+
+            double coord = (double)value;
+            return coord + param;
+        }
+    
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double translatedCoord = (double)value;
             return translatedCoord + 10;
         }
     }
