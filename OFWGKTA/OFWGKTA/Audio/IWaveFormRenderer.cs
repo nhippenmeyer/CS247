b783987732551928ad01d855dfe5449cf75fe11a
unknown <Ethan@Ethan-Laptop.(none)>
2012-02-21 10:46:46 -0800

    rudimentary audio recording implemented

---
+++ OFWGKTA/OFWGKTA/Audio/IWaveFormRenderer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OFWGKTA
+{
+    public interface IWaveFormRenderer
+    {
+        void AddValue(float maxValue, float minValue);
+    }
+}
